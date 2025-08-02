b95b7192be26c55ea005562dda4b2b3f5c24fbab
piyushkumar0707 <121piyush466mits@gmail.com>
2025-08-02 14:01:10 +0530

    Update files

--- part3/loops.js
+++ part3/loops.js
@@ -1,1 +1,39 @@
- 
+// let sum=0;
+// let i=0;
+//  while(i<5){
+//     sum+=i;
+//     i++;
+//  }console.log(sum); //output: 10
+
+//  let i=5;
+//  let countdown=[];
+//  while(i>0){
+//     countdown.push(i);
+//     i--;
+//  }console.log(countdown); //output: [5, 4, 3, 2, 1]
+     
+
+    // let tea=[];
+    // let userInput = prompt("Enter your fav tea:");
+    // while(userInput!=="stop"){
+    //     tea.push(userInput);
+    // }console.log(tea); //output: array of favorite teas     
+
+
+//     let total=0;
+//     let i=0;
+//     do{
+//         total+=i;
+//         i++;
+//      }
+//     while(i<=3)
+        
+//  console.log(total); //output: 6
+
+
+  
+let multipliedNumbers = [];
+let numbers=[2,4,6,8,10];
+for(let i=0;i<numbers.length;i++){
+    multipliedNumbers.push(numbers[i] * 2);
+}console.log(multipliedNumbers); // output: [4, 8, 12, 16, 20] 
