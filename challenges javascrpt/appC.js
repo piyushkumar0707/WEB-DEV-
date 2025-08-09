4d9e0a5d2d5639ddf9cc1b9186e5628bda6281f4
piyushkumar0707 <121piyush466mits@gmail.com>
2025-08-10 00:07:27 +0530

    Save local changes before pull

---
+++ challenges javascrpt/appC.js
@@ -0,0 +1,2 @@
+const mathOperations=require("./mathOperationsC.js")
+console.log(mathOperations.add(2,2));
---
+++ challenges javascrpt/appM.js
@@ -0,0 +1,12 @@
+// module export file
+
+//default import
+
+
+
+import {add, subtract} multiply from "./mathOperationsM.js";
+
+// named import
+import{ add, subtract} from "./mathOperationsM.js";
+console.log(multiply(2,2));
+console.log(add(2,2));
